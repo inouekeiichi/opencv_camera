53fb1c0508c6f513784781704fb77098cdfdf780
inouekeiichi <inoue.keiichis@gmail.com>
2018-03-04 16:11:25 +0900

    Add JavaCameraView.

--- app/src/main/java/com/phantom/android/opencvcamera/MainActivity.java
+++ app/src/main/java/com/phantom/android/opencvcamera/MainActivity.java
@@ -2,12 +2,65 @@ package com.phantom.android.opencvcamera;
 
 import android.app.Activity;
 import android.os.Bundle;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements CvCameraViewListener {
+    static {
+        System.loadLibrary("opencv_java3");
+    }
+
+    private CameraBridgeViewBase mCameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCameraView = (CameraBridgeViewBase) findViewById(R.id.javaCameraView2);
+        mCameraView.setCvCameraViewListener(this);
+
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(Mat inputFrame) {
+        return inputFrame;
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                // 読み込みが成功したらカメラプレビューを開始
+                case LoaderCallbackInterface.SUCCESS:
+                    mCameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
 }
